32fce9f472b2d6880df06273089b650bd1c7c89a
Torsten Raudssus <torsten@raudssus.de>
2014-09-25 12:13:21 +0000

    Import of GETTY/Dist-Zilla-Plugin-ReleaseOnlyWithLatest-0.003 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ReleaseOnlyWithLatest
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         GETTY/Dist-Zilla-Plugin-ReleaseOnlyWithLatest-0.003.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/ReleaseOnlyWithLatest.pm
+++ lib/Dist/Zilla/Plugin/ReleaseOnlyWithLatest.pm
@@ -2,79 +2,10 @@ package Dist::Zilla::Plugin::ReleaseOnlyWithLatest;
 BEGIN {
   $Dist::Zilla::Plugin::ReleaseOnlyWithLatest::AUTHORITY = 'cpan:GETTY';
 }
-{
-  $Dist::Zilla::Plugin::ReleaseOnlyWithLatest::VERSION = '0.002';
-}
-# ABSTRACT: Release the distribution only if specific modules are at latest state
-
-use Moose;
-
-with qw(
-  Dist::Zilla::Role::BeforeRelease
-);
-
-use version;
-use LWP::Simple;
-use Parse::CPAN::Packages::Fast;
-use File::Temp qw/ :POSIX /;
-use namespace::autoclean;
-
-around mvp_multivalue_args => sub {
-  my ($orig, $self) = @_;
-  $self->$orig, qw( package );
-};
-
-has package => ( is => 'ro', isa => 'ArrayRef[Str]', default => sub {[]} );
-has index => ( is => 'ro', isa => 'Str', default => sub {
-  'http://www.cpan.org'
-} );
-
-has index_url => ( is => 'ro', lazy => 1,
-  default => sub { shift->index.'/modules/02packages.details.txt.gz' }
-);
-
-sub before_release {
-  my ( $self ) = @_;
-  my $url = $self->index_url;
-  if (@{$self->package}) {
-    my $packages;
-    my $tempfile = tmpnam;
-    if (is_success(getstore($url,$tempfile))) {
-      $packages = Parse::CPAN::Packages::Fast->new($tempfile);
-    } else {
-      $self->log_fatal("Failed to download $url!");
-    }
-    for (@{$self->package}) {
-      my @packages = split(/,/,$_);
-      for my $package (@packages) {
-        my $version = version->parse($packages->package($package)->version);
-        $self->log_fatal("Wasn't able to find $package version on $url!") unless $version;
-        my $installed_version = version->parse($self->get_local_version($package));
-        if ($installed_version != $version) {
-          $self->log_fatal("You need $version of $package, but you have $installed_version!");
-        }
-      }
-    }
-  }
-}
- 
-sub get_local_version {
-  my ( $self, $module ) = @_;
-  require Module::Data;
-  my $v;
-  {
-    local $@;
-    eval {
-      my $m = Module::Data->new( $module );
-      $m->require;
-      $v = $m->version;
-      1
-    } or return;
-  };
-  return unless defined $v;
-  return version->parse($v) unless ref $v;
-  return $v;
-}
+# ABSTRACT: DEAD
+$Dist::Zilla::Plugin::ReleaseOnlyWithLatest::VERSION = '0.003';
+use strict;
+use warnings;
 
 
 1;
@@ -85,29 +16,17 @@ __END__
 
 =head1 NAME
 
-Dist::Zilla::Plugin::ReleaseOnlyWithLatest - Release the distribution only if specific modules are at latest state
+Dist::Zilla::Plugin::ReleaseOnlyWithLatest - DEAD
 
 =head1 VERSION
 
-version 0.002
+version 0.003
 
 =head1 SYNOPSIS
 
-  # Requires latest release from CPAN
-  [ReleaseOnlyWithLatest]
-  package = Dist::Zilla::PluginBundle::Author::YOU
-
-  # Requires latest release from a custom CPAN
-  [ReleaseOnlyWithLatest]
-  index = http://duckpan.org
-  package = Dist::Zilla::Plugin::UploadToDuckPAN
-
-  # Several ways to add several modules
-  [ReleaseOnlyWithLatest]
-  package = Package::A,Package::B
-  package = Package::C,Package::D  
-
 =head1 DESCRIPTION
+
+Duplicate Module, now removed, please use L<Dist::Zilla::Plugin::PromptIfStale>
 
 =head1 AUTHOR
 
